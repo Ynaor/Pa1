0bdfbee59412d37a471eedb9a3e1b361ccad259d
Yuval Naor <t-yuvalnaor@microsoft.com>
2022-03-17 18:55:44 +0200

    Continue channel logic

--- Channel/Channel/channel.cpp
+++ Channel/Channel/channel.cpp
@@ -1,21 +1,63 @@
 #include "channel.h"
 
 
-
 // ********************************************
 // **********  Socket Handling   **************
 // ********************************************
 
+// Global variables
+int gFlippedBits = 0;
+
+
+
 // Initializing the winsock, will throw an error and exit if failed
-void WinsockInit()
+void WinsockInit(WSADATA *wsaData)
 {
 	const auto api_ver = MAKEWORD(2, 2);
-	if (WSAStartup(api_ver, &wsaData) != NO_ERROR)
+	if (WSAStartup(api_ver, wsaData) != NO_ERROR)
 	{
-		std::cerr << "Winsock initialization failed\n";
+		std::cerr << "Winsock initialization failed\n" << WSAGetLastError();
 		exit(1);
 	}
 }
+
+
+void CreateSocket(SOCKET* aSocket)
+{
+	*aSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (*aSocket == INVALID_SOCKET)
+	{
+		std::cerr << "Could not create a new socket\n" << WSAGetLastError();
+		exit(1);
+	}
+}
+
+
+void InitAddresses(sockaddr_in *aReceiverAddr, sockaddr_in *aSenderAddr, sockaddr_in *aChannelAddr)
+{
+
+    // Channel
+	memset(aChannelAddr, 0, sizeof(*aChannelAddr));
+	aChannelAddr->sin_family = AF_INET;
+	aChannelAddr->sin_addr.s_addr = htonl(INADDR_ANY);  //  Auto assign IP
+	aChannelAddr->sin_port = 0;                         // Auto assign port
+
+	// Reciever
+	memset(aReceiverAddr, 0, sizeof(*aReceiverAddr));
+	aReceiverAddr->sin_family = AF_INET;
+	aReceiverAddr->sin_addr.s_addr = aChannelAddr->sin_addr.s_addr; // IP address of the server
+	aReceiverAddr->sin_port = 0;                                    // Auto assign port
+
+	// Sender
+	memset(aSenderAddr, 0, sizeof(*aSenderAddr));
+	aSenderAddr->sin_family = AF_INET;
+	aSenderAddr->sin_addr.s_addr =aChannelAddr->sin_addr.s_addr;   // IP address of the server
+	aSenderAddr->sin_port = 0;                                     // Auto assign port
+
+	return;
+}
+
+
 
 // Check the port value is in bounds, will throw an erro and exit if failed
 void ValidatePortNumber(int32_t aPortNumber)
@@ -29,19 +71,87 @@ void ValidatePortNumber(int32_t aPortNumber)
 
 
 // Will be generating random noise with given seed 
-void RandomNoise(double aProbability, char *aBuffer, unsigned int aRandSeed)
+void RandomNoise(int aProbability, char *aBuffer, unsigned int aRandSeed)
 {
-	int byte, bit;
 	unsigned char mask; 
+	std::default_random_engine generator(0); 
+	std::uniform_int_distribution<int> distribution(0,TWO_POWER_SIXTEEN);
+	long int randomNumber;
 
-	srand(aRandSeed); // using the random seed given as input to our program
-	for (byte = 0; byte < PACKET_SIZE_BYTES; byte++)
+	for (int byte = 0; byte < PACKET_SIZE_BYTES; byte++) 
 	{
 		mask = SINGLE_BIT_MASK;
-		for (bit = 0; bit < BITS_IN_BYTE; bit++)
+		for (int bit = 0; bit < BITS_IN_BYTE; bit++)
 		{
-			//TODO: complete the random noise (fully undesrstand the instructions given) using srand
+			randomNumber = distribution(generator);      // generating a random number in range [0, 2^16]
+			if (randomNumber < aProbability)
+			{
+				aBuffer[byte] = aBuffer[byte] ^ mask;    // flip the desired bit
+				gFlippedBits++;
+			}
+
+			mask <<= 1; // moving mask bit to the left
 		}
 	}
 }
 
+void DeterministicNoise(int aCycle, char* aBuffer)
+{
+	int counter = 0;
+	unsigned char mask;
+	
+	for (int byte = 0; byte < PACKET_SIZE_BYTES; byte++)
+	{
+		mask = SINGLE_BIT_MASK;
+		for (int bit = 0; bit < BITS_IN_BYTE; bit++)
+		{
+			if (counter == aCycle)
+			{
+				aBuffer[byte] = aBuffer[byte] ^ mask;    // flip the desired bit when we counter == cycle
+				gFlippedBits++;
+				counter = 0;                             // reset counter
+			}
+			counter++;
+		}
+	}
+}
+
+void BindServer(SOCKET* aMainSocket, sockaddr_in* aServerAddr)
+{
+	auto bindResult = bind(*aMainSocket, (SOCKADDR*)aServerAddr, sizeof(*aServerAddr));
+
+	if (bindResult == SOCKET_ERROR)
+	{
+		std::cerr << "Binding Failed. Exiting..\n" << WSAGetLastError();
+		exit(1);
+	}
+}
+
+
+void RunChannel()
+{
+	
+	SOCKET MainSocket = INVALID_SOCKET;
+	sockaddr_in receiverAddr;
+	sockaddr_in senderAddr;
+	sockaddr_in channelAddr;
+	WSADATA wsaData;                          // will contain the winsock data
+	
+	
+	// Initizliaing Winsock
+	WinsockInit(&wsaData);
+	CreateSocket(&MainSocket);
+	InitAddresses(&receiverAddr, &senderAddr, &channelAddr);
+	BindServer(&MainSocket, &channelAddr);
+
+
+	// main logic: Recieve a message, flip bits and send back to reciever
+
+
+	while (TRUE)
+	{
+		continue;
+	}
+
+
+}
--- Channel/Channel/main.cpp
+++ Channel/Channel/main.cpp
@@ -1,32 +1,11 @@
 #include "channel.h"
 
 
-
-
 int main(int argc, char* argv[])
 {
 
-	/*
-	SOCKET socket;
-	fd_set fd_reader_set;
-	timeval time;
+	RunChannel();
 
-	unsigned short channelPort;          // 16 bits representation for a port
-	unsigned short recieverPort;
-	double errorProb;
-	char* recieverIp;
-	*/
+	return 0;
 
-
-	// TODO: finish input validation
-	if ((argc > 4) | (argc < 2)) // Number of arguments iw wrong
-	{
-		std::cerr << "Number of arguments is wrong when calling channel.exe";
-	}
-	else
-	{
-		for (int i = 0; i < argc; i++) {
-
-		}
-	}
 }
