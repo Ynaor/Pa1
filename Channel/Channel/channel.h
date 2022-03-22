10f854be3c47bb3c88ce879393269f1151e1351e
Yuval Naor <t-yuvalnaor@microsoft.com>
2022-03-22 18:12:37 +0200

    Touchup

--- Channel/Channel/channel.h
+++ Channel/Channel/channel.h
@@ -1,3 +1,10 @@
+/*
+Authors:				Shachar Cohen (313416521) & Yuval Naor (312497084)
+Project:				Programming Assignment 1: Noisy Channel
+Project description:	Sender-Receiver communication through a noisy channel
+*/
+
+
 #pragma once
 
 #pragma comment(lib, "Ws2_32.lib")
